417b34a83e18e717fe26196ecdb214a8d776ee12
Shubham1322c <shubhamoffical1322@gmail.com>
2023-08-23 12:23:09 +0530

    jwsauthtoken added for user verification

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "HarshIsHere";
 
 
 // Create a User using: POST "/api/auth/createuser". No login required
@@ -15,19 +19,30 @@ router.post('/createuser', [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  // Check whether the user with this email exists already
   try {
+      // Check whether the user with this email exists already
     let user = await User.findOne({ email: req.body.email });
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists" })
     }
+
+    // encrypt userr password
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
       email: req.body.email,
     })
-    res.json(user)
+    const data = {
+        user:{
+            id : user.id
+        }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken})
     
   } catch (error) {
     console.error(error.message);
